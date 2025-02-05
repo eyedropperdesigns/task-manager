da1e03e8cbe01abf766c00f744a4c83fda5f3399
eyedropperdesigns <forte.mj@gmail.com>
2025-02-05 16:00:16 +1030

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID";
+const API_KEY = "YOUR_GOOGLE_API_KEY";
+const GOOGLE_SHEET_ID = "YOUR_SPREADSHEET_ID";
+const DISCOVERY_DOCS = [
+    "https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest",
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+    "https://sheets.googleapis.com/$discovery/rest?version=v4"
+];
+const SCOPES = "https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/spreadsheets";
